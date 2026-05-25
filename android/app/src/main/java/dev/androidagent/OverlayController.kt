4aadb6ad9a07c8874384b34ea346cce5a4d1ec41
am-will <freeme62410@gmail.com>
2026-05-25 13:45:09 -0600

    rename Android app to AgentLink

--- android/app/src/main/java/dev/androidagent/OverlayController.kt
+++ android/app/src/main/java/dev/androidagent/OverlayController.kt
@@ -672,7 +672,7 @@ class OverlayController(
         val settingsButton = iconButton(
             tokens = tokens,
             drawableRes = R.drawable.ic_settings_gear,
-            contentDescription = "AgentRelay settings",
+            contentDescription = "AgentLink settings",
             viewId = R.id.openclaw_header_settings_button,
             compact = true
         ) {
