e3c3438e5e7df8e8f672aef09a86f5b43d40b0cc
am-will <freeme62410@gmail.com>
2026-06-01 11:16:28 -0600

    android: bump internal release to 0.1.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "app.lynk"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
     }
 
     signingConfigs {
