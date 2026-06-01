0167badfa04fcecd8b714dda371a214847aa77b7
am-will <freeme62410@gmail.com>
2026-06-01 01:06:00 -0600

    chore: bump Android version and prune plans

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.lynk"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.1.2"
     }
 
     compileOptions {
